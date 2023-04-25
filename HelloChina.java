f46d708087a748b779474322b65fa3060c36a639
hjq8 <131655888+hjq8@users.noreply.github.com>
2023-04-25 21:41:59 +0800

    try

---
+++ HelloChina.java
@@ -0,0 +1,5 @@
+public class HelloChina{
+	public static void main(String[] arg){
+		System.out.println("Hello China!");
+}
+}
---
+++ day03_code/src/com/itmathcoperation/mathoperation.java
@@ -0,0 +1,7 @@
+package com.itmathcoperation;
+
+public class mathoperation {
+    public static void main(String[] args) {
+        System.out.println(3+2);
+    }
+}
